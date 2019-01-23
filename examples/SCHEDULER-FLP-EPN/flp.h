2b3dfb991b6969443e8550970980e8fe8d3bd1b7
Gvozden Neskovic <neskovic@compeng.uni-frankfurt.de>
2019-01-23 17:03:25 +0100

    clang-format, make sure only a single thread is sending data to flps (mutex)

--- examples/SCHEDULER-FLP-EPN/flp.h
+++ examples/SCHEDULER-FLP-EPN/flp.h
@@ -7,43 +7,34 @@
 #ifndef FAIRMQEXAMPLESCHEDULERFLPEPNFLP_H
 #define FAIRMQEXAMPLESCHEDULERFLPEPNFLP_H
 
-
-
-#include "FairMQDevice.h"
 #include <sstream>
+#include "FairMQDevice.h"
 
-namespace example_SCHEDULER_FLP_EPN
-{
+namespace example_SCHEDULER_FLP_EPN {
 
-class flp : public FairMQDevice
-{
-  public:
-    flp();
-    virtual ~flp();
+class flp : public FairMQDevice {
+ public:
+  flp();
+  virtual ~flp();
 
-  protected:
-    int* arrayofEpns; //just declare a pointer
+ protected:
+  int* arrayofEpns;  // just declare a pointer
 
+  const unsigned msBetweenSubtimeframes;
+  int amountEPNs;  // amount of epns in scheduler
+  uint64_t numEPNS;
+  int socket;
+  int myId;
+  int sTF;
+  int schedNum;
+  uint64_t startTime;
+  uint64_t programTime;
+  std::stringstream amountOfLostTfs1;
 
-
-    const unsigned msBetweenSubtimeframes;
-    int amountEPNs; //amount of epns in scheduler
-    uint64_t numEPNS;
-    int socket;
-    int myId;
-    int sTF;
-    int schedNum;
-    uint64_t startTime;
-    uint64_t programTime;
-    std::stringstream amountOfLostTfs1;
-
-
-
-    virtual void Run();
-    virtual void InitTask();
-    uint64_t getHistKey();
+  virtual void Run();
+  virtual void InitTask();
+  uint64_t getHistKey();
 };
-
 }
 
 #endif /* FAIRMQEXAMPLESCHEDULERFLPEPNFLP_H */
